3cd87ae6888c8a3a49edac90230ffad108e3bde1
Krunal Vala <krunalvala9@gmail.com>
2018-06-01 19:44:01 +0530

    Setup app for heroku

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -86,7 +86,6 @@ describe('GET /todos/:id', () => {
     });     
 
     it('should return 404 if todo not found', (done) => {
-        
         request(app)
             .get(`/todos/${new ObjectID().toHexString()}`)
             .expect(404)
