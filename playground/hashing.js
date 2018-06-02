95849365146406c9f310c25b78fef80cda0a600a
Krunal Vala <krunalvala9@gmail.com>
2018-06-03 02:54:58 +0530

    Hash user password before updating

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt =  require('bcryptjs');
 
-var data = {
-    name: 'Krunal'
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, 'asshole');
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log('hash :',hash);
+//     });
+// });
 
-var decode = jwt.verify(token, 'asshole');
+var hashedPassword = '$2a$10$fBckwXNdxKhSBDteK93/sOQmhNBf.aJ7TQeQZvkGffHCY3YjkbUza';
 
-console.log('Decode: ', decode);
+bcrypt.compare('123abc', hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+
+// var data = {
+//     name: 'Krunal'
+// };
+
+// var token = jwt.sign(data, 'asshole');
+
+// var decode = jwt.verify(token, 'asshole');
+
+// console.log('Decode: ', decode);
 
 // var message = 'I am user number 3';
 
