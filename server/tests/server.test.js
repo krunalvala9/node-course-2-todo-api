1a2679a7a302a11e355276f32a96a223f25115dc
Krunal Vala <krunalvala9@gmail.com>
2018-06-03 15:09:00 +0530

    Add tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,22 +4,13 @@ const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
+const {User} = require('./../models/user');
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
-const todos = [{
-    _id: new ObjectID(),
-    text: 'First test todo'
-}, {
-    _id: new ObjectID(),
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-}];
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
-beforeEach((done) => {
-    Todo.remove({}).then(() => {
-        return Todo.insertMany(todos);
-    }).then(() => done());
-});
+
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -143,7 +134,7 @@ describe('PATCH /todos/:id', () => {
         var id = todos[0]._id.toHexString();
         request(app)
             .patch(`/todos/${id}`)
-            .send({text: 'Updated text', completed: true})
+            .send({ text: 'Updated text', completed: true })
             .expect(200)
             .expect((res) => {
                 expect(res.body.todo.text).toBe('Updated text');
@@ -157,7 +148,7 @@ describe('PATCH /todos/:id', () => {
         var id = todos[1]._id.toHexString();
         request(app)
             .patch(`/todos/${id}`)
-            .send({text: 'Updated text', completed: false})
+            .send({ text: 'Updated text', completed: false })
             .expect(200)
             .expect((res) => {
                 expect(res.body.todo.text).toBe('Updated text');
@@ -165,5 +156,76 @@ describe('PATCH /todos/:id', () => {
                 expect(res.body.todo.completedAt).toNotExist();
             })
             .end(done);
+    });
+});
+
+describe('GET /users/me', () => {
+    it('should return user if authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body._id).toBe(users[0]._id.toHexString());
+                expect(res.body.email).toBe(users[0].email);
+            })
+            .end(done);
+    });
+
+    it('should return 401 if not authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .expect(401)
+            .expect((res) => {
+                expect(res.body).toEqual({});   
+            })
+            .end(done);
+    });
+});
+
+describe('POST /users', () => {
+    it('should create a user', (done) => {
+        var email = 'example@example.com';
+        var password = '123mnb!';
+        request(app)
+            .post('/users')
+            .send({email, password})
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+                expect(res.body._id).toExist();
+                expect(res.body.email).toBe(email);
+            })
+            .end((err) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findOne({email}).then((user) => {
+                    expect(user).toExist();
+                    expect(user.password).toNotBe(password);
+                    done();
+                });
+            });
+    });
+
+    it('should return validation errors if request is invalid', (done) => {
+        var email = 'assholegmail.com';
+        var password = '123456';
+        request(app)
+            .post('/users')
+            .send({email, password})
+            .expect(400)
+            .end(done);
+    });
+
+    it('should not create user if email in use', (done) => {
+        var email = users[0].email;
+        var password = 'abc123456';
+        request(app)
+            .post('/users')
+            .send({email, password})
+            .expect(400)
+            .end(done)
     });
 });
